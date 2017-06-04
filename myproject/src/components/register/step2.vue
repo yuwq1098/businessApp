cf75679bc28118f75a670264124ed25ed951f3d1
Geek.Yu <1098654043@qq.com>
2017-06-04 17:49:28 +0800

    页面样式调整

--- myproject/src/components/register/step2.vue
+++ myproject/src/components/register/step2.vue
@@ -1,7 +1,27 @@
 <template>
     <div id="step2">
-        <p>身份认证</p>
-        <router-link :to="{path:'/register/step3'}">前往成功页</router-link>
+        <div class="m-info-ipt">
+            <div class="m-ipt-line">
+                <span class="u-tit vital">真实姓名</span>
+                <input type="text" class="u-ipt" placeholder="与身份证一致" />
+            </div>
+            <div class="m-ipt-line">
+                <span class="u-tit vital">身份证号</span>
+                <input type="text" class="u-ipt" placeholder="与身份证一致" />
+            </div>
+            <div class="m-upload-box">
+                <span class="u-tit">上传身份证号</span>
+                <div class="m-upload">
+                    <div class="u-pic-box">
+                        <img src="../../common/img/u-add.png" alt="添加的小图标" class="u-add" />
+                        <p class="u-txt">上传图片</p>
+                    </div>
+                </div>
+            </div>
+            <div v-if="!isOK" class="bind-user m-btn-gray">提交信息，等待审核</div>
+            <router-link :to="{path:'/register/step3'}" v-if="isOK" class="bind-user m-btn-fill">提交信息，等待审核</router-link>
+            <router-link :to="{path:'/login'}" id="gotoLogin" class="u-lk f__fr">绑定已有账号</router-link>
+        </div>
     </div>
 </template>
 
@@ -11,7 +31,7 @@
         name: "step2",
         data () {
             return {
-
+                isOK : true,
             }
         },
         
